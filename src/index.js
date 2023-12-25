1a4cc0eb4c3855267a777a1c9655fbe607f82b46
Sagar ghare <gharesagar80@gmail.com>
2023-12-25 16:48:14 +0530

    Point 8 clustering and start:prod tested

--- src/index.js
+++ src/index.js
@@ -36,8 +36,5 @@ app.use((err, req, res, next) => {
   internalServerErrorResponse(res, INTERNAL_SERVER_ERROR)
 });
 
-app.listen(port, ()=> {
-  console.log(`Server is running on port ${port}`);
-});
 
 export default app;
